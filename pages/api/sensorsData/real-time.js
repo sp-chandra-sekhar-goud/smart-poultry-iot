b9562c1437d046d10c9174fba6dae9e004bf402e
sp-chandra-sekhar-goud <developer.spcsg@gmail.com>
2024-04-03 23:03:32 +0530

    .

---
+++ pages/api/sensorsData/real-time.js
@@ -0,0 +1,37 @@
+import { google } from 'googleapis';
+import { API_KEY } from '../utils.js/API_KEY';
+
+const apiKey = API_KEY; 
+const sheets = google.sheets({ version: 'v4', auth: apiKey });
+
+export default function handler(req, res) {
+  if (req.method === "GET") {
+    sheets.spreadsheets.values.get({
+      spreadsheetId: '1BD0oLF4mbVE-aO1VTPWXOURTvx4Xj73xjVfLfcxglos',
+      range: 'Sheet1',
+    })
+    .then(response => {
+      const values = response.data.values;
+      const totalRows = values.length;
+      const startRow = Math.max(totalRows - 10, 0);
+      const endRow = totalRows - 1;
+      const range = `Sheet1!A${startRow + 2}:H${endRow + 2}`; // +2 to skip the header row
+
+      // Fetch the last 100 rows using the calculated range
+      return sheets.spreadsheets.values.get({
+        spreadsheetId: '1BD0oLF4mbVE-aO1VTPWXOURTvx4Xj73xjVfLfcxglos',
+        range: range,
+      });
+    })
+    .then(response => {
+      const values = response.data.values;
+      res.status(200).json({ message: 'Data fetched successfully', data: values });
+    })
+    .catch(error => {
+      console.error('Error fetching data from Google Spreadsheet:', error);
+      res.status(500).json({ message: 'Internal server error' });
+    });
+  } else {
+    res.status(405).json({ message: 'Method Not Allowed' });
+  }
+}
--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,8 @@ import { useEffect, useState } from "react";
 import tempIcon from "../public/icons/temp.png";
 import humidityIcon from "../public/icons/humidity.png";
 import ammoniaIcon from "../public/icons/ammonia.png";
+import co2Icon from "../public/icons/co2.png";
+
 
 
 import { IoClose } from "react-icons/io5";
@@ -14,47 +16,56 @@ import { GrMenu } from "react-icons/gr";
 import { useMode } from "@/contexts/ModeContext";
 import NavLinks from "@/components/NavLinks";
 import ActuatorCard from "@/components/actuatorCard";
-import { useAuth } from "@/contexts/AuthContext";
 
 export default function Home() {
   const [loading, setLoading] = useState(true);
 
   const [tempData, setTempData] = useState([]);
   const [humidityData, setHumidityData] = useState([]);
-  const [carbonMonoxideData, setCarbonMonoxideData] = useState([]);
   const [co2Data, setCo2Data] = useState([]);
   const [ammoniaData, setAmmoniaData] = useState([]);
-  const [lpg, setLpg] = useState([]);
 
   const [lastUpdatedTime, setLastUpdatedTime] = useState("");
   const { mode, _ } = useMode();
 
-  const { user } = useAuth(); 
 
   useEffect(() => {
     async function fetchData() {
+    if(mode == "Real-Time Monitoring"){
       axios
-        .get("/api/sensorsData")
-        .then((response) => {
-          distributeData(response.data.data);
-          setLoading(false);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
+      .get("/api/sensorsData/real-time")
+      .then((response) => {
+        distributeData(response.data.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    }else{
+      axios
+      .get("/api/sensorsData/")
+      .then((response) => {
+        distributeData(response.data.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
     }
+    
+  }
 
-    fetchData();
+  fetchData();
 
-    const interval = setInterval(
-      () => {
-        fetchData();
-      },
-      mode === "Real-Time Monitoring" ? 60 * 1000 : 30 * 60000
-    );
+  const interval = setInterval(
+    () => {
+      fetchData();
+    },
+    mode === "Real-Time Monitoring" ? 60 * 1000 : 30 * 60000
+  );
 
-    return () => clearInterval(interval);
-  }, []);
+  return () => clearInterval(interval);
+}, []);
 
   const distributeData = (sensorData) => {
     sensorData.forEach((row) => {
@@ -63,17 +74,13 @@ export default function Home() {
         temperature,
         humidity,
         co2,
-        ammonia,
-        lpg,
-        carbonMonoxide,
+        ammonia
       ] = row;
       setTempData((prevData) => [...prevData, temperature]);
       setHumidityData((prevData) => [...prevData, humidity]);
-      setCarbonMonoxideData((prevData) => [...prevData, carbonMonoxide]);
       setCo2Data((prevData) => [...prevData, co2]);
       setAmmoniaData((prevData) => [...prevData, ammonia]);
-      setLpg((prevData) => [...prevData, lpg]);
-
+    
       const latestTimestamp =
         sensorData.length > 0 ? sensorData[sensorData.length - 1][0] : "";
       setLastUpdatedTime(latestTimestamp);
@@ -81,73 +88,82 @@ export default function Home() {
   };
 
   const [isNavOpen, setIsNavOpen] = useState(false);
+
   return (
     <Layout>
       {loading ? (
         <h1>Loading...</h1>
       ) : (
-        <div className="flex flex-col ">
-          <div className=" block md:hidden">
-            {isNavOpen ? (
-              <IoClose
-                className="absolute top-4 right-10 text-white block md:hidden text-[6vw] font-bold"
-                onClick={() => setIsNavOpen(false)}
+          <div className="flex flex-col ">
+            <div className=" block md:hidden">
+              {isNavOpen ? (
+                <IoClose
+                  className="absolute top-4 right-10 text-white block md:hidden text-[6vw] font-bold"
+                  onClick={() => setIsNavOpen(false)}
+                />
+              ) : (
+                <GrMenu
+                  className="absolute top-4 right-10 text-white block md:hidden text-[6vw] font-bold"
+                  onClick={() => setIsNavOpen(true)}
+                />
+              )}
+              <div className="absolute w-[95vw] h-[100vw] z-10 p-0">
+              {isNavOpen && <NavLinks />}
+              </div>
+            </div>
+            <div className="flex flex-col lg:flex-row justify-between">
+              <h1 className="text-lg px-4 py-2 lg:my-4">
+                Lastly updated at{" "}
+                <span className="font-bold">{lastUpdatedTime}</span>
+              </h1>
+              <h1 className="text-lg px-4 py-2 lg:my-4">
+                Mode: <span className="font-bold">{mode}</span>
+              </h1>
+            </div>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+              <Card
+                imgPath={tempIcon}
+                title={"Temperature"}
+                data={tempData}
+                unit={"Celsius (°c)"}
+                path={"/temperature"}
               />
-            ) : (
-              <GrMenu
-                className="absolute top-4 right-10 text-white block md:hidden text-[6vw] font-bold"
-                onClick={() => setIsNavOpen(true)}
+              <Card
+                imgPath={humidityIcon}
+                title={"Humidity"}
+                data={humidityData}
+                unit={"Percentage (%)"}
+                path={"/humidity"}
               />
-            )}
-            <div className="absolute w-[95vw] h-[100vw] z-10 p-0">
-            {isNavOpen && <NavLinks />}
+              <Card
+                imgPath={co2Icon}
+                title={"Carbon Dioxide"}
+                data={co2Data}
+                unit={"ppm"}
+                path={"/carbon-dioxide"}
+              />
+              <Card
+                imgPath={ammoniaIcon}
+                title={"Ammonia"}
+                data={ammoniaData}
+                unit={"Parts Per Million (ppm)"}
+                path={"/ammonia"}
+              />
+              <ActuatorCard
+                iconName={"FaFan"}
+                item={"Fan"}
+                status={"On"}
+              />
+              <ActuatorCard
+                iconName={"FaLightbulb"}
+                item={"Light"}
+                status={"Off"}
+              />
+              
             </div>
           </div>
-          <div className="flex flex-col lg:flex-row justify-between">
-            <h1 className="text-lg px-4 py-2 lg:my-4">
-              Lastly updated at{" "}
-              <span className="font-bold">{lastUpdatedTime}</span>
-            </h1>
-            <h1 className="text-lg px-4 py-2 lg:my-4">
-              Mode: <span className="font-bold">{mode}</span>
-            </h1>
-          </div>
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-            <Card
-              imgPath={tempIcon}
-              title={"Temperature"}
-              data={tempData}
-              unit={"Celsius (°c)"}
-              path={"/temperature"}
-            />
-            <Card
-              imgPath={humidityIcon}
-              title={"Humidity"}
-              data={humidityData}
-              unit={"Percentage (%)"}
-              path={"/humidity"}
-            />
-            <Card
-              imgPath={ammoniaIcon}
-              title={"Ammonia"}
-              data={ammoniaData}
-              unit={"Parts Per Million (ppm)"}
-              path={"/ammonia"}
-            />
-            <ActuatorCard
-              iconName={"FaFan"}
-              item={"Fan"}
-              status={"On"}
-            />
-            <ActuatorCard
-              iconName={"FaLightbulb"}
-              item={"Light"}
-              status={"Off"}
-            />
-            
-          </div>
-        </div>
-      )}
+        )      
+      }
     </Layout>
   );
 }
